c6fa6ae0e7b63f10a010c9932e014f16d5da7c6d
Magnus <getthisent@yahoo.com>
2025-03-20 22:41:46 -0400

    Setup GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace 'elevateddesignz' and 'BullCircle' with actual GitHub info
 export default defineConfig({
-  base: '/BullCircle/',  // ⚠️ repo name with slashes
-  plugins: [react()]
+  base: '/BullCircle/',  // <- Add this line!
+  plugins: [react()],
 })
